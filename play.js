06f3116033917364dfc00a7ef3c29e700458c324
saurabhdabas <developerdabas@gmail.com>
2022-02-16 22:14:11 +0000

    Adding the movements to the snake

--- play.js
+++ play.js
@@ -1,4 +1,32 @@
 const {connect} = require("./client")
 
 console.log("Connecting ...");
-connect();
+let connInput = connect();
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+const handleUserInput = function (key) {
+  // to terminate the game
+  if (key === '\u0003') {
+    process.exit();
+  }
+  // Moving the snake on the board 
+  if (key === 'w') {
+    connInput.write("Move: up")
+  }
+  if (key === 's') {
+    connInput.write("Move: down")
+  }
+  if (key === 'a') {
+    connInput.write("Move: left")
+  }
+  if (key === 'd') {
+    connInput.write("Move: right")
+  }
+};
+setupInput();
