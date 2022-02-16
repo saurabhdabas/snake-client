27301713e0cf72c2af8baad895ba9d187a55f88e
saurabhdabas <developerdabas@gmail.com>
2022-02-16 22:28:32 +0000

    Refactored

--- play.js
+++ play.js
@@ -1,32 +1,6 @@
 const {connect} = require("./client")
 
+const setupInput = require("./input");
 console.log("Connecting ...");
-let connInput = connect();
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-const handleUserInput = function (key) {
-  // to terminate the game
-  if (key === '\u0003') {
-    process.exit();
-  }
-  // Moving the snake on the board 
-  if (key === 'w') {
-    connInput.write("Move: up")
-  }
-  if (key === 's') {
-    connInput.write("Move: down")
-  }
-  if (key === 'a') {
-    connInput.write("Move: left")
-  }
-  if (key === 'd') {
-    connInput.write("Move: right")
-  }
-};
+connect();
 setupInput();
