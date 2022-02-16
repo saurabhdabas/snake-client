04786c7bd344b57320e57f73cd6ad68b50cfe2de
saurabhdabas <developerdabas@gmail.com>
2022-02-16 22:28:10 +0000

    Created a separate file to track user inputs

---
+++ input.js
@@ -0,0 +1,33 @@
+const {connect} = require("./client")
+
+let connInput = connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+const handleUserInput = function (key) {
+  // to terminate the game
+  if (key === '\u0003') {
+    process.exit();
+  }
+  // Moving the snake on the board 
+  if (key === 'w') {
+    connInput.write("Move: up")
+  }
+  if (key === 's') {
+    connInput.write("Move: down")
+  }
+  if (key === 'a') {
+    connInput.write("Move: left")
+  }
+  if (key === 'd') {
+    connInput.write("Move: right")
+  }
+};
+
+module.exports = setupInput;
