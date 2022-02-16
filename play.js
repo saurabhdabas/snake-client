64571a346aa5bbaf0ca80a37469bd6ec7ec224b1
saurabhdabas <developerdabas@gmail.com>
2022-02-16 20:22:30 +0000

    Refactored it using module.exports

--- play.js
+++ play.js
@@ -1,20 +1,4 @@
-const net = require("net");
+const connect = require("./client")
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host:"165.227.47.243",
-    port: "50541"
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log("Connection with the server esatablished");
-  });
-  return conn;
-};
-
-console.log("Connecting ...");
 connect();
+console.log("Connecting ...");
